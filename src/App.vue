893279d8c13dc33aac73b7125c166a2b3cd7d2fd
AlyNour99 <72401446+AlyNour99@users.noreply.github.com>
2024-03-27 14:57:07 +0200

    test1

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
 
-  <div class="container">  <div class="row d-flex">
-    <div class="col-sm-6"> 
-    <BarChart :labels="['Consumables','Banks', 'Healthcare', 'Real Estate', 'Textiles & Durables', 'Energy', 'Contracting','NBFS', 'Transportation', 'Basic Resources', 'Communucation','Industrials']"
-    :values="[-1.31,-0.23,1,1.32,1.38,2.64,3.77,4.22,4.93,5.99,6.02,6.6]" title="AveragePerformance/Sector"/>
-    </div>
-    
-    <div class="col-sm-6">
-    <BarChart :labels="['Orascom', 'CIB', 'Eastern','Abu Qir', 'Wadi Kom Ombo']" :values="[11.9, 10.2, 2.6, 1.1,0.1]" title="Top 5 gainers"/>
-    <TodayBar dailyChange ='-1.5' currentPoints='5674' YtDate='5.62'/>
-    <PieChart :gainers='25' :losers='20'/>
-    </div>
-  </div></div>
+  <div class="container">
+    <div class="row d-flex">
+      <div class="col-sm-12">
+        <BarChart
+          :labels="[
+            'Consumables',
+            'Banks',
+            'Healthcare',
+            'Real Estate',
+            'Textiles & Durables',
+            'Energy',
+            'Contracting',
+            'NBFS',
+            'Transportation',
+            'Basic Resources',
+            'Communucation',
+            'Industrials',
+          ]"
+          :values="[
+            -1.31, -0.23, 1, 1.32, 1.38, 2.64, 3.77, 4.22, 4.93, 5.99, 6.02,
+            6.6,
+          ]"
+          title="Average Performance/Sector"
+        />
+      </div>
 
+      <div class="col-sm-10">
+        <BarChart
+          :labels="['Orascom', 'CIB', 'Eastern', 'Abu Qir', 'Wadi Kom Ombo']"
+          :values="[11.9, 10.2, 2.6, 1.1, 0.1]"
+          title="Top 5 gainers"
+        />
+        <TodayBar dailyChange="-1.5" currentPoints="5674" YtDate="5.62" />
+        <PieChart :gainers="25" :losers="20" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import BarChart from './components/BarChart.vue'
-import PieChart from './components/PieChart.vue'
-import TodayBar from './components/TodayBar.vue'
-
+//import ModalA from './components/ModalA.vue'
+import BarChart from "./components/BarChart.vue";
+import PieChart from "./components/PieChart.vue";
+import TodayBar from "./components/TodayBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     BarChart,
     PieChart,
-    TodayBar
-  }
-}
+    TodayBar,
+  },
+};
 </script>
 
 <style>
 #app {
+  font-size: large;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/ModalA.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>model content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(150, 80, 70, 0.5);
+  width: 100%;
+  height: 100%;
+}
+</style>
