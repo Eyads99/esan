d32da0572c282b5533d801839ac4821726173cea
AlyNour99 <72401446+AlyNour99@users.noreply.github.com>
2024-03-28 15:22:03 +0200

    V280324

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-
   <div class="container">
     <div class="row d-flex">
       <div class="col-sm-6">
@@ -23,18 +21,28 @@
             -1.31, -0.23, 1, 1.32, 1.38, 2.64, 3.77, 4.22, 4.93, 5.99, 6.02,
             6.6,
           ]"
-          title="AveragePerformance/Sector"
+          title="Average performance/sector"
         />
       </div>
 
       <div class="col-sm-6">
         <BarChart
-          :labels="['Orascom', 'CIB', 'Eastern', 'Abu Qir', 'Wadi Kom Ombo']"
+          :labels="[
+            'Orascom Construction',
+            'CIB',
+            'Eastern',
+            'Abu Qir',
+            'Wadi Kom Ombo',
+          ]"
           :values="[11.9, 10.2, 2.6, 1.1, 0.1]"
           title="Top 5 gainers"
         />
-        <TodayBar dailyChange="-1.5" currentPoints="5674" YtDate="5.62" />
-        <PieChart :gainers="25" :losers="20" />
+        <div class="chart-container2">
+          <PieChart :gainers="25" :losers="20" />
+        </div>
+        <div class="chart-container1">
+          <TodayBar dailyChange="-1.5" currentPoints="5674" YtDate="5.62" />
+        </div>
       </div>
     </div>
   </div>
@@ -56,13 +64,36 @@ export default {
 </script>
 
 <style>
+.gap {
+  height: -90px;
+}
+
+.chart-container1 {
+  position: absolute;
+  top: 550px;
+  left: 115px;
+  width: 40%;
+  height: 10%;
+}
+
+.chart-container2 {
+  position: absolute;
+  top: 410px;
+  left: 800px;
+  width: 40%;
+  height: 10%;
+}
+.bar {
+  width: 20px;
+  margin-right: 5px;
+}
 #app {
   font-size: large;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, cursive, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
 }
 </style>
