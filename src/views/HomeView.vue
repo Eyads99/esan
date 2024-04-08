3a4e36a3a1ea266087857404b6ae7e18b258c095
Eyad Shaban <eyadshaban99@gmail.com>
2024-04-08 16:24:28 +0200

    Sector Perfmance updater

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
   <sideDrawer/>
     <v-main >
       <h1>EGX Today</h1>
-      <v-btn @click="changeValues">Top / Bottom 5</v-btn>
+      <v-btn @click="reverseOrder">Top / Bottom 5</v-btn>
          --
       <v-btn-toggle
           v-model="indexSelection"
@@ -26,7 +26,7 @@
            cols="12" md="6" justify="center">
            <v-card elevated class="card-margin">
             <BarChart style="height: 65vh"
-              title="Average performance/sector"
+              title="Sectoral Performance"
               :labels = sectors
               :values= sectorChg
             />
@@ -67,7 +67,7 @@
         </div>
         <div v-if="gainers">
           <v-card style="height: 40vh" elevated class="card-margin">
-            <h3>Gainers and losers</h3>
+            <h3>Market Movement</h3>
            <PieChart style=" height: 100%" :gainers="gainers" :losers="losers" ID="GL" />
           </v-card>
           </div>
@@ -84,6 +84,7 @@
 <script>
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "/src/firebase/init";
+//import { toRaw } from 'vue';
 import BarChart from "/src/components/BarChart.vue";
 import PieChart from "/src/components/PieChart.vue";
 import TodayBar from "/src/components/TodayBar.vue";
@@ -96,8 +97,8 @@ export default {
       idxPointShow: null,//this data is to be shown to user
       idxYtDate: null, //this data is to be shown to user
       idxDailyChgShow: null, //this data is to be shown to user
-      topstockChgs: null, //used as prop in charts has chaging data
-      topStockNames: null, //used as prop in charts has chaging data
+      topstockChgs: null, //used as prop in charts has changing data
+      topStockNames: null, //used as prop in charts has changing data
       topCount: 5, //get top x given value i.e 10 -> top ten; -5 -> bottom 5
       allStockNamesOrder: null,
       allChgValuesOrder: [],
@@ -109,6 +110,7 @@ export default {
       indexSelection: 30, // default set to EGX30
       indexChgs: {}, // gets % change for all indices
       indexPoints: {}, // gets points for all indices
+      sectorComponents: {}, // Key array pairs for sectors and component stocks
       EGX30TickerList : ["ABUK","ADIB","ALCN","AMOC","BTFH","COMI","EAST","EFID","HRHO","EFIH","EKHO","EKHOA","SWDY","ESRS",
       "FWRY","GBCO","HELI","JUFO","MASR","MFPC","ORAS","ORHD","ORWE","PHDC","CCAP","SKPC","TMGH","ETEL","SUGR","PHAR","ISPH"],
       EGX70TickerList : ["ACAP","ACGC","ADPC","AFDI","AIH","AMER","ARAB","ASPI","ATLC","ATQA","BINV","CERA","CICH","CIEB","CIRA",
@@ -120,7 +122,7 @@ export default {
     };
   },
   methods: {
-    changeValues() {
+    reverseOrder() {
       this.topCount *= -1
       this.indexChg()
       /*if (this.topCount > 0) {
@@ -134,7 +136,7 @@ export default {
 
     indexChg()
     {
-      console.log(this.indexSelection)
+      //deals with todayBar data
       const idx = "EGX" + this.indexSelection
       this.idxDailyChgShow = ((this.indexChgs[idx])*100).toFixed(2);
       this.idxPointShow = this.indexPoints[idx];
@@ -147,11 +149,8 @@ export default {
       case "EGX30":
         //set stocksNeeded to all stocks that are a value in the dict allStocksChgToday and are in the list EGX30TickerList
         //get all key value pairs where a key is in EGX30TickerList
-
         stocksNeeded = Object.fromEntries(Object.entries(this.allStocksChgToday).filter(([key]) => this.EGX30TickerList.includes(key)));
-
-
-        //this.allStocksChgToday.filter(x => this.EGX30TickerList.includes(x))//error hear checking for enttries not keys
+        //this.allStocksChgToday.filter(x => this.EGX30TickerList.includes(x))//error here checking for entries not keys
         break;
       case "EGX70":
         stocksNeeded = Object.fromEntries(Object.entries(this.allStocksChgToday).filter(([key]) => this.EGX70TickerList.includes(key)));
@@ -164,7 +163,7 @@ export default {
       const topstocks = Object
           .entries(stocksNeeded) // create Array of Arrays with [key, value]
           .sort(([, a],[, b]) => (this.topCount>0 ? b - a : a - b)) // sort by value, descending (b-a)
-          .slice(0,Math.abs(this.topCount)) // return only the first topCount elements of the intermediate result
+          .slice(0, Math.abs(this.topCount)) // return only the first topCount elements of the intermediate result
         //reverse order of top10
       topstocks.reverse()
 
@@ -174,6 +173,35 @@ export default {
       this.gainers = Object.values(stocksNeeded).filter((x) => x > 0).length
       this.losers = Object.values(stocksNeeded).filter((x) => x < 0).length//stocksNeeded.length - this.gainers //0 is considered a loss
 
+      //mange sectors
+      let keys = Object.keys(this.sectorComponents)
+      console.log(keys,"keys")
+      //loop over all keys in sectorComponents and get the average value of all stocks in the array that are in stocksNeeded
+      this.sectorChg = []
+      
+
+      for (let i = 0; i < keys.length; i++)
+      //for (const [key, value] of Object.entries(this.sectorComponents)) 
+      {  
+        let key = keys[i]
+        let stocks = this.sectorComponents[key]
+        //console.log("sector stocks",Object.entries(stocks))
+        let filteredStocks = stocks.filter(x => Object.keys(stocksNeeded).includes(x)) // get all keys that are in the current sector and stocksNeeded
+        //console.log(filteredStocks)
+        if (filteredStocks.length > 0)
+          this.sectorChg[key] = this.getAvgChg(filteredStocks, stocksNeeded) 
+        
+      }
+      //remove all sectors that have a value of null
+      this.sectorChg = Object.fromEntries(Object.entries(this.sectorChg).filter(([key]) => this.sectorChg[key] !== null));
+      
+      //make sectorChg in descending order by values
+      this.sectorChg = Object.entries(this.sectorChg).sort((a, b) => a[1] - b[1]).reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+      this.sectors = Object.keys(this.sectorChg)
+      this.sectorChg = Object.values(this.sectorChg)
+      //make all sectors First letter capital
+      this.sectors = this.sectors.map( x => {return x.charAt(0).toUpperCase() + x.slice(1)})
     },
 
     getAvgChg(stockNames, allChgs) {
@@ -197,7 +225,7 @@ export default {
     mounted(){
 
     //let docRef = doc(db, 'stocks', 'EGX30') unneeded after this.indexChg func
-    let secdocRef = doc(db, "info", "industry")  
+  let secdocRef = doc(db, "info", "industry")  
 
    /* getDoc(docRef).then(doc => { //gets todays trading data for stocks
       var today = new Date()
@@ -227,8 +255,16 @@ export default {
         ((doc.data()[today] - doc.data()[yesterday]) / doc.data()[yesterday]) *100).toFixed(2); // unneeded to do this.indexChg func
     });*/
 
-    let docRef = doc(db, "stocks", "changes") //get stock with last trading day's changes
+  let docRef = doc(db, "stocks", "changes") //get stock with last trading day's changes
     
+  //docRef = doc(db, "stocks", "idxChanges")
+  getDoc(doc(db, "stocks", "idxChanges")).then(doc => {this.indexChgs = doc.data()});
+    
+  //docRef = doc(db, "stocks", "todayIndices")
+  getDoc(doc(db, "stocks", "todayIndices")).then(doc => {    
+    this.indexPoints = doc.data()      
+    this.indexSelection = 30});
+
     getDoc(docRef).then((doc) => {
       this.allStocksChgToday = doc.data()//gets dict with all stocks with last trading day's chgs
 
@@ -241,16 +277,17 @@ export default {
         this.allChgValuesOrder.push(((doc.data()[key])*100).toFixed(2))//makes array with all chgs in decreasing order  
     }   
 
-        /*this.topStockNames = this.allStockNamesOrder.slice(0,10)// get last 30 elements
-        this.topstockChgs = this.allChgValuesOrder.slice(0,10)// get last 30 elements
-
-        this.gainers = this.allChgValuesOrder.filter((x) => x > 0).length //get the number of stocks that increased
-        this.losers = this.allChgValuesOrder.length - this.gainers*/ //unneed after this.indexChg() func
-
         //below is for next doc for getting sectors and their component stocks
         return getDoc(secdocRef) 
       }).then((doc2) => { //get sector change
-          
+        
+        // Iterate over each field in the document
+        
+        //Object.assign(this.sectorComponents,JSON.parse(JSON.stringify(doc2.data())))
+        this.sectorComponents=doc2.data()
+        console.log( Object.keys(this.sectorComponents))
+              
+
         const sectorsDoc = Object.keys(doc2.data())
 
         for (let i = 0; i < sectorsDoc.length; i++) {
@@ -265,26 +302,12 @@ export default {
         this.sectorChg = Object.values(this.sectorChg)
         //make all sectors First letter capital
         this.sectors = this.sectors.map( x => {return x.charAt(0).toUpperCase() + x.slice(1)})
-
+        this.indexChg() 
       });     
     
        //get index daily change data
-    docRef = doc(db, "stocks", "idxChanges")
-    getDoc(docRef).then(doc => {
-      console.log(doc.data())// TODO see why this must be logged to work!!!
-      this.indexChgs = doc.data()
 
-      });
       
-    docRef = doc(db, "stocks", "todayIndices")
-    getDoc(docRef).then(doc => {
-      //console.log(doc.data())// TODO see why this must be logged to work!!!
-      this.indexPoints = doc.data()      
-      this.indexSelection = 30
-      this.indexChg()  
-    });
-      
-      console.log("end of mount")
   }
 }
 </script>
