b73cbd94205a1bddfbe493ffbdd1e036768c5842
AlyNour99 <72401446+AlyNour99@users.noreply.github.com>
2024-04-10 12:01:27 +0200

    Attempt1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <v-app> 
+  <v-app>
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>|
       <router-link to="/IndexToday">Index Today</router-link>
     </nav>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
   }),
-}
+};
 </script>
