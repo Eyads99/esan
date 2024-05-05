fec97d67982f58df9c8c892a412f2e00f1ec2b06
Eyad Shaban <eyadshaban99@gmail.com>
2024-05-05 09:49:56 +0300

    Refactored

--- src/router/index.js
+++ src/router/index.js
@@ -21,12 +21,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/IndexToday.vue')
+    component: () => import('../views/IndexToday.vue')
   },
   {
     path: '/priceCompare',
     name: 'priceCompare',
-    component: () => import(/* webpackChunkName: "about" */ '../views/PriceCompareView.vue')
+    component: () => import('../views/PriceCompareView.vue')
   }
 ]
 
