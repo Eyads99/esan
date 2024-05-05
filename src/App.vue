5c5acb993e4fd37a2f1f756cc80db35028439501
Eyad Shaban <eyadshaban99@gmail.com>
2024-05-05 09:50:09 +0300

    refactor

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/IndexToday">Index Today</router-link>|
+      <router-link to="/about">About</router-link> |
+      <router-link to="/IndexToday">Index Today</router-link> |
       <router-link to="/priceCompare">Asset Compare</router-link>
     </nav>
     <v-main>
