3012b88c82662c0280174c49ac58ef59216b824a
Eyad Shaban <eyadshaban99@gmail.com>
2024-05-31 12:43:00 +0300

    added PE ratio

--- src/views/AssetView.vue
+++ src/views/AssetView.vue
@@ -4,14 +4,17 @@
 <h2>{{ getCompanyName($route.params.ticker) }}</h2>
 <v-card elevated class="card-margin">
                 <v-layout justify-center>                  
-                             Daily:{{this.chg}}
+                             Price Today: EGP {{this.todayPrice}}
+                             Daily Change:{{this.chg}}%
                              Earning per share: {{this.eps}}
                              Index: {{this.index}}
                              Industry: {{this.industry}}
                              Margin: {{this.margin}}
                              Profit: {{this.profit}}
                              price to book ratio: {{this.ptb}}
-                             Rev: {{this.rev}}                             
+                             Rev: {{this.rev}}  
+                             Dividend Yield: {{this.divYield}}%
+                             Price/Earnings ratio: {{(this.todayPrice/this.eps).toFixed(2)}}                         
                 </v-layout>
               </v-card>
               <PriceHistoryChart :assetsNames="[$route.params.ticker]" :normalize=false 
@@ -40,6 +43,8 @@ export default{
       profit: null,
       ptb: null,
       rev: null,
+      divYield: null,
+      todayPrice: null,
       startDate: new Date('2008-01-01'),
       endDate: new Date(), 
       dateRange: [new Date('2018-01-01'), new Date()],
@@ -52,14 +57,17 @@ export default{
       this.companyData = data.default
       this.fetchData().then(() =>
       {    
+        console.log(this.data)
         this.chg = ((this.data.chg)*100).toFixed(2)
-        this.eps = ((this.data.eps)*100).toFixed(2)
+        this.eps = ((this.data.eps)).toFixed(2)
         this.index = this.data.index
         this.industry = this.data.industry
         this.margin = (this.data.margin).toFixed(2)
         this.profit = (this.data.profit)
         this.ptb = (this.data.ptb).toFixed(2)
         this.rev = (this.data.rev).toFixed(2)
+        this.divYield = (this.data.div_yield).toFixed(2)
+        this.todayPrice = Object.values(this.data).slice(-11)[0]
       }
       )
       
